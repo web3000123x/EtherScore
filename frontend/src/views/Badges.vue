9f06a4ff5ff07eb50a4936fae78e15fe093f7648
CryptoMarabout <sami.barbara94fake@gmail.com>
2021-06-21 01:51:23 +0100

    add basic backend query

--- frontend/src/views/Badges.vue
+++ frontend/src/views/Badges.vue
@@ -11,13 +11,27 @@
           This page will display the list of all existing badges 
           with a button claimable... 
         </p>
+        
+        <p> Data request from backend : {{ info }} </p>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
     name: 'Badges',
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get(process.env.VUE_APP_BASE_URL + 'ping')
+        .then(response => (this.info = response.data))
+    }
   }
 </script>
--- frontend/src/views/Vaults.vue
+++ frontend/src/views/Vaults.vue
@@ -10,13 +10,27 @@
         <p class="subheading font-weight-regular">
           This page will do the interface to interact with EtherScore smart contracts
         </p>
+        
+        <p> Data request from backend : {{ info }} </p>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
     name: 'Vaults',
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get(process.env.VUE_APP_BASE_URL + 'ping')
+        .then(response => (this.info = response.data))
+    }
   }
 </script>
