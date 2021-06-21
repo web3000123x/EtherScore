ccc3d9236d61444f1eda1543a96d9135b86df333
CryptoMarabout <sami.barbara94fake@gmail.com>
2021-06-21 02:43:53 +0100

    Add basic card component

--- frontend/src/views/Vaults.vue
+++ frontend/src/views/Vaults.vue
@@ -1,19 +1,38 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
+  <v-container >
+    <v-row class="text-center" align="center" justify="center">
+        <v-card
+          elevation="25"
+          color="secondary"
+          style="margin-top:15px; max-width:450px; border-radius: 20px;
+           padding: 1.5rem;  border: 1px solid; color: white; font-weight: 500;
+           opacity: 0.95;"
+        >
+          
+          <h1 class="display-2 mb-3">
+            <v-icon color="white">mdi-treasure-chest</v-icon>
+            Vaults
+            <v-icon color="white">mdi-treasure-chest</v-icon>
+          </h1>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Vaults
-        </h1>
+          <p class="subheading font-weight-regular">
+            This page will do the interface with EtherScore smart contracts
+          </p>
 
-        <p class="subheading font-weight-regular">
-          This page will do the interface to interact with EtherScore smart contracts
-        </p>
-        
-        <p> Data request from backend : {{ info }} </p>
-      </v-col>
-    </v-row>
+          <p>
+            Data request from backend : 
+          </p>
+          <v-spacer></v-spacer>
+          <v-chip
+            color="white"
+            class="text-subtitle-1 font-weight-light"
+            outlined
+            text-color="white"
+          >
+            {{ info }}
+          </v-chip>
+        </v-card>
+      </v-row>
   </v-container>
 </template>
 
