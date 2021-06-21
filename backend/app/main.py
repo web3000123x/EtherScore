1ceb888dd2c9965e9e4080d874411de41ab27dde
Edouard <outin.edouard@gmail.com>
2021-06-21 18:24:28 +0200

    endpoints for claimable & all badges

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-import os, datetime
+import os, datetime, json
 
 from fastapi import Request, FastAPI
 
@@ -8,6 +8,46 @@ DEBUG = os.environ.get('DEBUG')
 # instantiate the app
 app = FastAPI(debug=DEBUG, title="EtherScore-backend")
 
+badge0 = {
+    "id" : 0,
+    "name": "Badge Number 0",
+    "description": "This is the badge 0 description",
+    "owner" : "Toto",
+    "address" : "0x999999999999999999",
+    "tags": ["uniswap", "experience"],
+    "image_url": "https://i.redd.it/rq36kl1xjxr01.png",
+    "conditions": [
+        {
+            "protocol": "uniswap",
+            "rule": "null"
+        },
+        {
+            "protocol": "uniswap",
+            "rule": "null"
+        }
+    ]
+}
+
+badge1 = {
+    "id" : 1,
+    "name": "Badge Number 1",
+    "description": "This is the badge 1 description",
+    "owner" : "Toto",
+    "address" : "0x9888888888999999999",
+    "tags": ["uniswap", "transaction"],
+    "image_url": "https://i.redd.it/rq36kl1xjxr01.png",
+    "conditions": [
+        {
+            "protocol": "uniswap",
+            "rule": "null"
+        },
+        {
+            "protocole": "uniswap",
+            "rule": "null"
+        }
+    ]
+}
+
 ######################################
 # Define the different app endpoints #
 ######################################
@@ -16,3 +56,17 @@ app = FastAPI(debug=DEBUG, title="EtherScore-backend")
 @app.get("/ping")
 def ping():
     return str(datetime.datetime.now())
+
+# List all existing badges
+@app.get("/all_badges")
+def all_badges():
+    return [badge0, badge1]
+
+# Retrieve claimable badges for a given address
+@app.post("/claimable_badges")
+async def claimable_badges(request: Request):
+    content = await request.json()
+    wallet_address = str(content["wallet_address"])
+    print("User address: " + wallet_address)
+    id_badge0 = 0
+    return [id_badge0]
