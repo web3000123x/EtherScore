3488bb90d86cfec14bbaeba8cab40c270f615b63
CryptoMarabout <sami.barbara94fake@gmail.com>
2021-07-05 20:26:24 +0100

    last fix minting bug

--- frontend/src/views/Badges.vue
+++ frontend/src/views/Badges.vue
@@ -285,7 +285,7 @@ import json from '../BadgeTokenFactory.json'
         const web3 = new Web3(window.ethereum, null, {transactionConfirmationBlocks: 1})
         var contract = new web3.eth.Contract(this.myJson.abi ,'0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B');
         console.log(contract);
-        var query = await contract.methods.requestBadgeTokenMinting("0")
+        var query = await contract.methods.requestBadgeTokenMinting("2")
         var res = await query.send({from: this.$store.state.address})
         console.log(res)
         var eventsList = await contract.getPastEvents( 'QueryRequest', { fromBlock: 'latest', toBlock: 'latest' } )
@@ -295,7 +295,7 @@ import json from '../BadgeTokenFactory.json'
           });
         for (var queryRequest in eventsList) {
           console.log(queryRequest)
-          await this.callOracle("0")
+          await this.callOracle("2")
           var oracle = await contract.methods.updateBadgeTokenMinting(eventsList[queryRequest]["returnValues"]["_requestID"],String(this.oracleReturn[0]))
           console.log(oracle)
           var res2 = await oracle.send({from: this.$store.state.address})
@@ -307,7 +307,7 @@ import json from '../BadgeTokenFactory.json'
         .then(function(events){
           return events[0]["returnValues"]["0"]
           });
-        var mintQuery = await contract.methods.mintBadgeToken("0");
+        var mintQuery = await contract.methods.mintBadgeToken("2");
         var res3 = await mintQuery.send({from: this.$store.state.address})
         this.displayClaimed = true
         console.log(res3);
